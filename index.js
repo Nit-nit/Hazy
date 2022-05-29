f44011316ac6676215fa7ee0591862312bb7b807
Nit-nit <94877880+Nit-nit@users.noreply.github.com>
2022-05-30 00:08:52 +0530

    yare yare daze, i just made a jojo reference
    i want some warm breast milk
    i removed a line which wasnt important for the code

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // Require the necessary discord.js classes
-const Discord = require('discord.js');
 const config = require('./config.json');
-const { Client, Intents } = require('discord.js');
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const {
+  Discord,
+  Client,
+  Intents
+} = require('discord.js');
+
+const client = new Client({
+  intents: [
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGES
+  ]
+});
 
 //custom classes
 const ping = "<@913559840780091453>";
